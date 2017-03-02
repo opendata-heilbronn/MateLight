82f95012265327cc8cd2b321c7c625ed5cbfde91
Patrick Hahn <patricksilashahn@gmx.net>
2017-03-02 20:14:26 +0000

    testScroll.js now accepts parameters

--- testScroll.js
+++ testScroll.js
@@ -3,16 +3,23 @@ const Hardware = require('./src/materixHardware.module');
 const ScrollText = require('./src/scrollText.module');
 const Pixel = require('./src/models/pixel.model');
 
-var hardware = new Hardware({serialDevice: 'COM11', baudRate: 500000});
+var hardware = new Hardware({serialDevice: '/dev/ttyUSB0', baudRate: 500000});
 var materix = new Materix({width: 4, height: 2, orientation: "landscape", handler: hardware});
 var scrollText = new ScrollText({setPixelMethod: materix.setPixel, updateMethod: materix.send});
 
+var params = process.argv;
+params.shift();
+params.shift();
+
 setTimeout(function(){
     scrollText.setText({
-        text: "Coworking Space Heilbronn",
+        text: params.join(' '),
         endX: 19,
+        //scrollSpeed: 68,
         scrollSpeed: 68,
-        textColor: [128, 128, 128],
-        //backColor: [0, 16, 0]
+        textColor: [255, 0, 0],
+        backColor: [0, 0, 32]
     });
+    for(var i = 0; i < 20; i++)
+        materix.setPixel(i, 7, [0, 0, 32]);
 }, 100);
