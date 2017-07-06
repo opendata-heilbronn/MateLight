77a0416c1f0e26908f55b5e83c5d246240361112
harmoniemand <jonathan.guenz@outlook.com>
2017-07-06 23:42:21 +0200

    test

--- api/scripts/testScroll.js
+++ api/scripts/testScroll.js
@@ -15,28 +15,28 @@ console.log(options);
 
 // ---------------------------------------------------------------------------
 
-const Materix =     require('./../materix.module');
-const Hardware =    require('./../materixHardware.module');
-const ScrollText =  require('./../scrollText.module');
-const Pixel =       require('./../models/pixel.model');
+const Materix = require('./../materix.module');
+const Hardware = require('./../materixHardware.module');
+const ScrollText = require('./../scrollText.module');
+const Pixel = require('./../models/pixel.model');
 
-var hardware = new Hardware({serialDevice: options.device, baudRate: 500000});
+var hardware = new Hardware({ serialDevice: options.device, baudRate: 500000 });
 var width = options.width, height = options.height;
-var materix = new Materix({width: width, height: height, orientation: options.orientation, handler: hardware});
-var scrollText = new ScrollText({setPixelMethod: materix.setPixel, updateMethod: materix.send, onEnd: scroll});
+var materix = new Materix({ width: width, height: height, orientation: options.orientation, handler: hardware });
+var scrollText = new ScrollText({ setPixelMethod: materix.setPixel, updateMethod: materix.send, onEnd: scroll });
 
 var params = process.argv;
 params.shift(); //remove first two elements
 params.shift();
 
 function scroll() {
-    for(var y = 0; y < height*4; y++)
-        for(var x = 0; x < width*5; x++)
+    for (var y = 0; y < height * 4; y++)
+        for (var x = 0; x < width * 5; x++)
             materix.setPixel(x, y, [0, 0, 32]);
     scrollText.setText({
         text: options.text,
         startY: 0,
-        endX: 14,
+        endX: options.width * 5 -1,
         //scrollSpeed: 68,
         scrollSpeed: 68,
         textColor: [255, 0, 0],
