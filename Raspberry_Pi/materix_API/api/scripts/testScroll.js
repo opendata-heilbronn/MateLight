ae4ab5794656ea745212407f45826142d264191d
Patrick Hahn <patricksilashahn@gmx.net>
2019-02-22 01:17:56 +0000

    35c3 modifications to scroll text

--- Raspberry_Pi/materix_API/api/scripts/testScroll.js
+++ Raspberry_Pi/materix_API/api/scripts/testScroll.js
@@ -10,19 +10,28 @@ if(!args.text) {
     return;
 }
 
+function init() {
+for (var x = 0; x < args.width * 5; x++) {
+    for (var y = 0; y < args.height * 4; y++) {
+        materix.setPixel(x, y, [0, 64, 0])
+    }
+}
+}
+
 function scroll() {
     for (var y = 0; y < args.height * 4; y++)
         for (var x = 0; x < args.width * 5; x++)
             materix.setPixel(x, y, [0, 0, 32]);
     scrollText.setText({
         text: args.text,
-        startY: 0,
+        startY: 4,
         endX: args.width * 5 -1,
         //scrollSpeed: 68,
-        scrollSpeed: 68,
-        textColor: [255, 0, 0],
-        backColor: [0, 0, 32]
+        scrollSpeed: 100,
+        textColor: [255, 255, 255],
+        backColor: [0, 64, 0]
     });
 }
 
-setTimeout(scroll, 100);
+setTimeout(scroll, 200);
+setTimeout(init, 150);
